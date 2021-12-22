c00e6efd45a3f46e3457a3359e19647187b2e121
Chris Martin <chris@crunchycloud.io>
2021-12-22 15:09:44 +1000

    initial version

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import { ethers } from 'ethers';
 import { io, Socket } from "socket.io-client";
 import Game from './Game';
 import './App.css';
+import public_thumb from './img/public_thumb.png';
+import tracer_thumb from './img/tracer_thumb.png';
+import vip_thumb from './img/vip_thumb.png';
 
-const BASE_URL = 'http://localhost:3000'
+// const BASE_URL = 'http://localhost:3000'
+const BASE_URL='http://35.244.99.101'
 
 type Player = {
   x: number,
@@ -15,16 +19,31 @@ type Player = {
   currentRoom: string
 }
 
+type RoomData = {
+  name: string,
+  background: string,
+  players: Record<string, Player>
+}
+
+const emptyRoom = {
+  name: 'Spore Village',
+  background: 'https://i.ibb.co/HFj2bKP/Screen-Shot-2021-12-21-at-10-30-43-am-cropped.png',
+  players: {}
+}
+
 const toRedactedAddress = (address: string) => {
   return `${address.slice(0, 4)}...${address.slice(-4)}`
 }
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState<string>();
+  const [playerName, setPlayerName] = useState<string>('');
+  const [avatar, setPlayerAvatar] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+  const [messages, setMessages] = useState<{ sender: string, message: string}[]>([]);
   const [sporeverseKey, setSporeverseKey] = useState(window.sessionStorage.getItem('key'));
-  const [roomSocket, setRoomSocket] = useState<Socket>()
   const [baseSocket, setBaseSocket] = useState<Socket>()
-  const [players, setPlayers] = useState<Record<string, Player>>({})
+  const [roomData, setRoomData] = useState<RoomData>(emptyRoom)
 
   const connectWalletHandler = async () => {
     // @ts-ignore
@@ -39,6 +58,7 @@ function App() {
       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
       console.log('Found an account! address', accounts[0]);
       setCurrentAccount(accounts[0]);
+      return accounts[0]
     } catch (error) {
       console.error(error)
     }
@@ -47,18 +67,24 @@ function App() {
   const logout = () => {
     window.sessionStorage.removeItem('key');
     setSporeverseKey(null)
+    setRoomData(emptyRoom)
   }
 
   useEffect(() => {
     const _baseSocket = io(BASE_URL);
     _baseSocket.on('tick', data => {
-      setPlayers(data);
+      setRoomData(data);
+    })
+    _baseSocket.on('message', data => {
+      setMessages(previous => [data, ...previous]);
     })
     setBaseSocket(_baseSocket)
   }, [])
 
   const joinSocketRoom = useCallback(async (roomName: string) => {
     console.log('ATTEMPTING TO JOIN SOCKET ROOM')
+
+    setMessages([])
 
     if(sporeverseKey) {
 
@@ -67,6 +93,8 @@ function App() {
       console.log('EMITTING JOIN')
       if(baseSocket) {
         baseSocket.emit('join', { key: sporeverseKey, roomName });
+      } else {
+        console.log('NO BASE SOCKET')
       }
     } else {
       console.log('NO SPOREVERSE KEY')
@@ -74,38 +102,38 @@ function App() {
   }, [sporeverseKey, baseSocket])
 
   const loginToSporeverseHandler = useCallback(async () => {
-    // @ts-ignore
-    const { ethereum } = window;
+      // @ts-ignore
+      const { ethereum } = window;
 
-    if(!ethereum) {
-      alert('Please install metamask')
-    }
+      if(!ethereum) {
+        alert('Please install metamask')
+      }
 
-    await connectWalletHandler()
+      const account = await connectWalletHandler()
 
-    const { nonce } = await fetch(`${BASE_URL}/nonce/${currentAccount}`).then(res => res.json());
+      const { nonce } = await fetch(`${BASE_URL}/nonce/${account}`).then(res => res.json());
 
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
 
-    const message = `Log into the Sporeverse: ${nonce}`
-    const signature = await signer.signMessage(message);
+      const message = `Log into the Sporeverse: ${nonce}`
+      const signature = await signer.signMessage(message);
 
-    const { key } = await fetch(`${BASE_URL}/login`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        address: currentAccount,
-        signature,
-        message
-      })
-    }).then(res => res.json())
+      const { key } = await fetch(`${BASE_URL}/login`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          address: account,
+          signature,
+          message
+        })
+      }).then(res => res.json())
 
-    window.sessionStorage.setItem('key', key)
-    setSporeverseKey(key);
-  }, [currentAccount])
+      window.sessionStorage.setItem('key', key)
+      setSporeverseKey(key);
+  }, [])
 
   const handlePlayerMoved = useCallback((direction: { x: number, y: number }) => {
     if(baseSocket) {
@@ -113,75 +141,123 @@ function App() {
     }
   }, [baseSocket])
 
-  // const loginToSporeverseButton = useCallback(() => {
-  //   return (
-  //     <button onClick={loginToSporeverseHandler} disabled={Boolean(sporeverseKey)} className='cta-button connect-wallet-button'>
-  //       { sporeverseKey && currentAccount ? `Logged in as ${toRedactedAddress(currentAccount)}` : 'Log into the sporeverse' }
-  //     </button>
-  //   )
-  // }, [sporeverseKey, currentAccount, loginToSporeverseHandler])
-
-  useEffect(() => {
-    connectWalletHandler();
-    if(currentAccount && sporeverseKey) {
-    }
-    // joinSocketRoom('public');
-    // const socket = io("https://server-domain.com");
-  }, [])
-
-
   useEffect(() => {
     if(currentAccount && sporeverseKey) {
       // connectToSocket();
       joinSocketRoom('public')
     }
     // refresh socket connection
-  }, [currentAccount, sporeverseKey])
+  }, [currentAccount, sporeverseKey, joinSocketRoom])
 
-  // useEffect(() => {
-  //   // refresh socket connection
-  // }, [currentAccount, sporeverseKey])
+  useEffect(() => {
+    connectWalletHandler()
+    // refresh socket connection
+  }, [])
 
+  const updatePlayerName = useCallback(() => {
+    if(baseSocket) {
+      baseSocket.emit('updateName', playerName)
+    }
+  }, [baseSocket, playerName])
 
+  const updatePlayerAvatar = useCallback(() => {
+    if(baseSocket) {
+      baseSocket.emit('updateAvatar', avatar)
+    }
+  }, [baseSocket, avatar])
+
+  const sendMessage = useCallback(() => {
+    if(baseSocket) {
+      baseSocket.emit('sendMessage', message)
+      setMessage('')
+    }
+  }, [baseSocket, message])
 
   return (
     <div className='main-app'>
       <div>
-        <h1 style={{ display: 'inline-block', marginRight: '10px' }}>Hello Spore</h1>
+        <h1 style={{ display: 'inline-block', marginRight: '10px', color: 'white' }}>Hello Spore</h1>
           <button onClick={loginToSporeverseHandler} disabled={Boolean(sporeverseKey)} className='cta-button connect-wallet-button'>
             { sporeverseKey && currentAccount ? `Logged in as ${toRedactedAddress(currentAccount)}` : 'Log into the sporeverse' }
           </button>
-          <button onClick={() => joinSocketRoom('public')}>
-            join public room
-          </button>
-          <button onClick={() => joinSocketRoom('vip')}>
-            join private room
-          </button>
-          <button onClick={() => logout()}>
+          <button className="logout-button" onClick={() => logout()}>
             logout
           </button>
-
           {/* AKA */}
           {/* <input>  </> */}
-        </div>
-      <div>
-        {
-          currentAccount && sporeverseKey ? (
+      </div>
+      <div style={{ display: 'flex' }}>
+        <div>
+          { baseSocket ? (
             <Game
-              playerAddress={currentAccount}
-              players={players}
+              players={roomData.players}
+              name={roomData.name}
+              background={roomData.background}
               handlePlayerMoved={handlePlayerMoved}
             />
-          ) : null
-        }
+          ) : (
+            <img
+            src={emptyRoom.background}
+            width="1100px"
+            height="auto"
+            alt="public sporeverse"
+          ></img>
+          ) }
+
+        </div>
+        <div style={{ color: 'white', marginLeft: '20px', marginRight: '20px', display: 'flex', flexDirection: 'column' }}>
+          <img
+            style={{ marginBottom: '30px', cursor: 'pointer' }}
+            width="100px"
+            src={public_thumb}
+            alt="public_area_thumbnail"
+            onClick={() => joinSocketRoom('public')}
+          />
+          <img
+            style={{ marginRight: '30px', cursor: 'pointer'  }}
+            width="100px"
+            src={vip_thumb}
+            alt="vip_area_thumbnail"
+            onClick={() => joinSocketRoom('vip')}
+          />
+          <img
+            style={{ marginRight: '30px', cursor: 'pointer'  }}
+            width="100px"
+            src={tracer_thumb}
+            alt="tracer_area_thumbnail"
+            onClick={() => joinSocketRoom('tracer')}
+          />
+        </div>
+        <div style={{color: 'white'}} id="profile">
+          <p><strong style={{fontSize: '24px'}}>Spore Profile</strong></p>
+          <strong style={{fontSize: '18px'}}>Name</strong>
+          <br/>
+          <input type="text" value={playerName} onChange={(event) => setPlayerName(event.target.value)}/>
+          <button onClick={() => updatePlayerName()}>save</button>
+          <br/>
+          <br/>
+          <strong style={{fontSize: '18px'}}>Avatar</strong>
+          <br/>
+          <input type="text" value={avatar} onChange={(event) => setPlayerAvatar(event.target.value)}/>
+          <button onClick={updatePlayerAvatar} >save</button>
+          <br/>
+          <br/>
+          <p><strong style={{fontSize: '24px'}}>Spore Chat ({roomData.name})</strong></p>
+          <strong style={{fontSize: '18px'}}>Message</strong>
+          <br/>
+          <textarea cols={25} rows={6} value={message} onChange={(event) => setMessage(event.target.value)}/>
+          <button onClick={() => sendMessage()}>send</button>
+          <br/>
+          {messages.map(message => (
+            <div style={{ marginBottom: '5px' }}>
+              <strong>{message.sender}:</strong><br/>
+              <span>{message.message}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
 }
-
-// const LoginToSporeverseButton: React.FC<{
-//   account?: string,
-//   handleLogin:
-// }> = ({})
 
 export default App;
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,5 +1,4 @@
-import { stringify } from 'querystring'
-import React, { useCallback, useEffect, useState, useMemo } from 'react'
+import { useEffect } from 'react'
 
 type Player = {
   x: number,
@@ -10,47 +9,44 @@ type Player = {
   currentRoom: string
 }
 
-const GameArea = ({
-  playerAddress,
+const Game = ({
   players,
+  background,
   handlePlayerMoved
 }: {
-  playerAddress: Player['address'],
   players: Record<string, Player>,
+  name: string,
+  background: string,
   handlePlayerMoved: (direction: { x: number, y: number }) => void
 }) => {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [height, setHeight] = useState(window.innerHeight);
-
   useEffect(() => {
-    console.log('ADDING EVENT LISTENER')
 
-    window.addEventListener("keydown", function (event: any) {
+    const handler = function (event: any) {
       if (event.defaultPrevented) {
         return; // Do nothing if the event was already processed
       }
 
-      const delta = 7
+      const movementSpeed = 12
 
       switch (event.key) {
         case "Down": // IE/Edge specific value
         case "ArrowDown":
-          handlePlayerMoved({ y: delta, x: 0 })
+          handlePlayerMoved({ y: movementSpeed, x: 0 })
           // Do something for "down arrow" key press.
           break;
         case "Up": // IE/Edge specific value
         case "ArrowUp":
-          handlePlayerMoved({ y: -delta, x: 0 })
+          handlePlayerMoved({ y: -movementSpeed, x: 0 })
           // Do something for "up arrow" key press.
           break;
         case "Left": // IE/Edge specific value
         case "ArrowLeft":
-          handlePlayerMoved({ x: -delta, y: 0 })
+          handlePlayerMoved({ x: -movementSpeed, y: 0 })
           // Do something for "left arrow" key press.
           break;
         case "Right": // IE/Edge specific value
         case "ArrowRight":
-          handlePlayerMoved({ x: delta, y: 0 })
+          handlePlayerMoved({ x: movementSpeed, y: 0 })
           // Do something for "right arrow" key press.
           break;
         default:
@@ -59,43 +55,34 @@ const GameArea = ({
 
       // Cancel the default action to avoid it being handled twice
       event.preventDefault();
-    }, true)
+    }
+
+    window.addEventListener("keydown", handler, true)
   }, [handlePlayerMoved])
 
   return (
     <>
+      <img
+        src={background}
+        width="1100px"
+        height="auto"
+        alt="public sporeverse"
+      ></img>
+
       {(Object.values(players).map(player => (
         <div
           key={player.address}
           style={{
             position: 'absolute',
             left: player.x,
-            top: player.y
+            top: player.y,
           }}
         >
-          <img alt={player.address} height="80" width="80" src={player.avatar}></img>
-          <span>{player.address.slice(0, 6)}...</span>
+          <img alt={player.address} width="90" src={player.avatar}></img>
+          <div style={{ background: 'rgba(255, 255, 255, 0.8)', padding: '4px', textAlign: 'center' }}>{player.label || `${player.address.slice(0, 6)}...`}</div>
         </div>
       )))}
     </>
-  )
-}
-
-const Game = ({
-  playerAddress,
-  players,
-  handlePlayerMoved
-}: {
-  playerAddress: string,
-  players: Record<string, Player>
-  handlePlayerMoved: (direction: { x: number, y: number }) => void
-}) => {
-  return (
-    <GameArea
-      playerAddress={playerAddress}
-      players={players}
-      handlePlayerMoved={handlePlayerMoved}
-    />
   )
 }
 
